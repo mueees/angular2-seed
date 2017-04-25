a044bc96f0686398bf9683911f5f430a38f2d181
vitalii.mishchenko <vitalii_mishchenko_cw@bmc.com>
2017-04-25 13:17:09 +0300

    add aot build 3

--- src/main.aot.ts
+++ src/main.aot.ts
@@ -1,6 +1,9 @@
 import {platformBrowser} from "@angular/platform-browser";
+import {enableProdMode} from "@angular/core";
 import {AppModuleNgFactory} from "./ngfactory/src/app/app.module.ngfactory";
 
 import "./assets/scss/index.scss";
+
+enableProdMode();
 
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
